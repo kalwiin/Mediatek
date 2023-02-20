63dd714c0849f78892dd1f7a0e470824700917e7
ZayloXx <107071481+ZayloXx@users.noreply.github.com>
2023-02-20 10:17:33 +0100

    Tâche 4 : ajouter l'accès avec authentification (4h) — Le back office ne doit être accessible qu'après authentification : un seul profil administrateur doit avoir le droit d'accès. Pour gérer l'authentification, utiliser Keycloak. — Il doit être possible de se déconnecter, sur toutes les pages (avec un lien de déconnexion).

---
+++ wamp64/www/mediatekformation/src/Form/PlaylistType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Playlist;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlaylistType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la playlist*',
+                'required' => true
+
+            ])
+            ->add('description', TextareaType::class, [
+            'label' => 'Description de la playlist',
+            'required' => false
+            ])
+            ->add('submit', SubmitType::class, [
+            'label' => 'Enregistrer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Playlist::class,
+        ]);
+    }
+}
